5f54fa21418e322874282b60d5907df245904e79
MacobJagyar <jacobmagyar95@gmail.com>
2022-08-20 15:15:19 -0400

    method clean up, formatting clean up

--- MuscleCircus/Program.cs
+++ MuscleCircus/Program.cs
@@ -5,9 +5,8 @@ using System.ComponentModel;
 
 Clubs Detroit = new Clubs();
 
-
     StartOfLoop:
-    Console.WriteLine("Welcome to Muscle Circus of " + Locations.Detroit.ToString() + "!");
+    Console.WriteLine("Welcome to "+ Detroit.Name + "!");
     Console.WriteLine("\nPlease select an option: ");
     Console.WriteLine("\n1. Check in a member");
     Console.WriteLine("2. Add a member");
@@ -41,10 +40,13 @@ switch (menuChoice)
             break;
         case 2:
             Console.Clear();
-            Console.WriteLine("Is this person going to be a Grand or Single member? (Grand/Single): ");
+            Console.WriteLine("Add a member menu -");
+            Console.WriteLine("____________\n");
+            Console.WriteLine("Is this person going to be a Grand or Single member? (grand/single) ");
+            Console.WriteLine("(Enter \"x\" to return to the main menu)");
             string tierChoice = Console.ReadLine().ToLower();
-
-            if (tierChoice == "grand")
+        
+        if (tierChoice == "grand")
             {
                 GrandMember NewMember = new GrandMember();
                 Detroit.AddMember(NewMember);
@@ -54,12 +56,17 @@ switch (menuChoice)
                 SingleMember NewMember = new SingleMember();
                 Detroit.AddMember(NewMember);
             }
+            else if (tierChoice == "x")
+            {
+            Console.Clear();
+            goto StartOfLoop;
+            }
             else
             {
                 Console.Clear();
-                Console.WriteLine("Not a valid input, try again.");
-                Thread.Sleep(2000);
-        goto case 2;
+                Console.WriteLine("Not a valid input. Press any key to start over");
+                Console.ReadKey();
+                goto case 2;
             }
             break;
         case 3:
@@ -70,24 +77,36 @@ switch (menuChoice)
         Detroit.PrintBillOfSales();
             break;
         case 5:
+        Console.Clear();
+        Console.WriteLine("Log off menu");
+        Console.WriteLine("____________\n\n");
         Console.WriteLine("Are you sure you want to log off?");
 
         string logOffInput = Console.ReadLine().ToLower();
 
         if (logOffInput == "y")
         {
-            Console.WriteLine("Goodbye!");
+            Console.Clear();
+            Console.WriteLine("Log off menu");
+            Console.WriteLine("____________\n\n");
+            Console.WriteLine("Goodbye! \n\nI am your father’s brother’s nephew’s cousin’s former roommate.");
             Thread.Sleep(2500);
             Environment.Exit(0);
         }
         else if (logOffInput == "n")
         {
-            goto StartOfLoop;
+            Console.Clear();
+            Console.WriteLine("Log off menu");
+            Console.WriteLine("____________\n\n");
+            Console.WriteLine("Log off cancelled");
+
         }
         else
         {
-            Console.WriteLine("That wasn't a valid choice. Going to main menu...");
-            Thread.Sleep(2500);
+            Console.Clear();
+            Console.WriteLine("Log off menu");
+            Console.WriteLine("____________\n\n");
+            Console.WriteLine("That wasn't a valid choice.");
             break;
         }
         break;
